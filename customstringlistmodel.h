e8628ca88ac94b997d136de8c85f29d837a517f5
JTP0501 <josh.patinof@gmail.com>
2024-05-28 21:46:40 +0800

    Added Struct Task

--- customstringlistmodel.h
+++ customstringlistmodel.h
@@ -2,6 +2,19 @@
 #define CUSTOMSTRINGLISTMODEL_H
 
 #include <QStringListModel>
+#include <QMimeData>
+#include <QList>
+
+// Define a struct to represent a task
+struct Task {
+    QString taskName;
+    QString semester;
+    QString course;
+    QString weight;
+    double totalScore;
+    double score;
+    bool complete;
+};
 
 class CustomStringListModel : public QStringListModel
 {
@@ -10,11 +23,21 @@ class CustomStringListModel : public QStringListModel
 public:
     explicit CustomStringListModel(QObject *parent = nullptr);
 
+    // Overridden functions
     Qt::ItemFlags flags(const QModelIndex &index) const override;
     Qt::DropActions supportedDropActions() const override;
     QStringList mimeTypes() const override;
-    QMimeData *mimeData(const QModelIndexList &indexes) const override;
+    QMimeData* mimeData(const QModelIndexList &indexes) const override;
     bool dropMimeData(const QMimeData *data, Qt::DropAction action, int row, int column, const QModelIndex &parent) override;
+
+    // Custom functions to interact with tasks
+    void addTask(const Task &task);
+    void removeTask(int index);
+    Task getTask(int index) const;
+
+private:
+    QList<Task> m_tasks;
 };
 
 #endif // CUSTOMSTRINGLISTMODEL_H
+
